b15ee49f7a018d58f9aa243ff4ded154185e90f5
Aka <95154437+Fujino-aka@users.noreply.github.com>
2022-10-24 13:17:40 +0530

    inits

---
+++ init.py
@@ -0,0 +1,38 @@
+
+ME_MSG = """
+<b>Name :</b> <code>{}</code>
+<b>User Name :</b> <code>{}</code>
+<b>User ID :</b> <code>{}</code>
+<b>Language :</b> <code>{}</code>
+<b>Child Mode :</b> <code>{}</code>
+<b>Total Guess :</b> <code>{}</code>
+<b>Correct Guess :</b> <code>{}</code>
+<b>Wrong Guess :</b> <code>{}</code>
+<b>Unfinished Guess :</b> <code>{}</code>
+<b>Total Questions :</b> <code>{}</code>
+"""
+
+LANG_CODE = {
+    'en': 'English',
+    'ar': 'Arabic',
+    'cn': 'Chinese',
+    'de': 'German',
+    'es': 'Spanish',
+    'fr': 'French',
+    'il': 'Hebrew',
+    'it': 'Italian',
+    'jp': 'Japanese',
+    'kr': 'Korean',
+    'nl': 'Dutch',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ru': 'Russian',
+    'tr': 'Turkish',
+    'id': 'Indonesian',
+}
+
+LANG_MSG = """
+Change Playing Language.
+<b>NOTE : This does not change the Bot language.</b>
+<b>Current Language :</b> <pre>{}</pre>
+"""
