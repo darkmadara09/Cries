fbc3aa4b65339eadc35d1750800639be16966a13
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-01-01 13:16:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-MONGO_HOST = "#Put you mongo url"
-BOT_TOKEN = "#Put your bot token"
+MONGO_HOST = "mongodb+srv://ixbotmongodb:RadheRadhe@cluster0.ga1q2fn.mongodb.net/?retryWrites=true&w=majority"
+BOT_TOKEN = "6978305332:AAFjnkqawnTmFOscKbXLMqkfNs4Dnn8ZMFg"
